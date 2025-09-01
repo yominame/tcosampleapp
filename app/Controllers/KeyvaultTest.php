f39f2207d449098c99ebd8d76451339e33639e1f
Yogesh Vargaonkar <yvargaonkar@extern.csatp.com>
2025-09-01 04:13:35 -0700

    updated version v4

--- app/Controllers/KeyvaultTest.php
+++ app/Controllers/KeyvaultTest.php
@@ -7,7 +7,7 @@ class KeyvaultTest extends BaseController
     public function index()
     {
         // Read the secret from environment variable
-        $secret = env('database-password');
+        $secret = env('database.password');
 
         // Pass it to the view
         return view('keyvault_view', ['secret' => $secret]);
